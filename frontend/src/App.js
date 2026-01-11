fb668a532050d26de8e3c30b2ccc2864854f25b1
Marcos Tavarez <mtavarez0625@Marcoss-MacBook-Air.local>
2026-01-11 09:05:00 -0500

    Use production backend API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./App.css";
 import React from "react";
 
-const API = "http://localhost:5050/api/bags";
+const API = "https://baggage-tracker-backend.onrender.com/api/bags";
 
 export default function App() {
   const [error, setError] = useState("");
